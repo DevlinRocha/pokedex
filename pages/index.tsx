d922125d0e9b2d18580c57eceade379544fb5143
Devlin <devlincodes@gmail.com>
2023-01-18 18:11:04 -0800

    Fix spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="p-10 text-[#222] font-['Poppins']">
-        <header className="flex flex-col items-center pb-3">
+        <header className="flex flex-col items-center pb-20">
           <h1 className="flex items-center">
             <Image src={Pokeball} alt="Pokéball" width={36} height={36} />
 
@@ -21,13 +21,13 @@ export default function Home() {
             </span>
           </h1>
 
-          <span className="w-[746px] text-xl text-center font-['Open_Sans'] leading-[27px] pb-3.5">
+          <span className="w-[746px] text-xl text-center font-['Open_Sans'] leading-[27px] p-3">
             Welcome to the Pokédex challenge! The goal is to build out the UI
             from this Figma. Then you’ll make a request to the pokéapi endpoint
             to populate it with the data you receive.
           </span>
 
-          <span className="bg-[#E6E6E6] p-0.5 rounded font-semibold leading-6 font-mono">
+          <span className="bg-[#E6E6E6] rounded font-semibold leading-6 font-mono">
             https://pokeapi.co/api/v2/pokemon/
           </span>
         </header>
